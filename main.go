03eec89aea82b076ccb400181982d6c5d1a1b43e
toto <50374594+toine08@users.noreply.github.com>
2025-01-03 09:21:43 +0000

    ajout de la fonctionnalité de suppression des doublons et amélioration de l'affichage des résultats

--- main.go
+++ main.go
@@ -8,16 +8,25 @@ import (
 
 
 func main() {
+	separator :=fmt.Sprintf("\n----------------------------------------------\n")
 	nums := []int{1, 3, 2, 5, 4}
 	nums_nonSorted := []int{1, 3, 2, 5, 4}
+	nums_Duplicates := []int{1,3,2,2,4,4,5,2}
 
 	fmt.Printf("Array before insertion sort: %v \n", nums)
 	algorithm.Insertion_sort(nums)
 	fmt.Printf("Array after insertion sort: %v \n", nums)
-	fmt.Println("\n----------------------------------------------\n")
+	fmt.Println(separator)
+
 	nums = nums_nonSorted
 	fmt.Printf("Array before bubble sort: %v \n", nums)
 	algorithm.Bubble_sort(nums)
 	fmt.Printf("Array after bubble sort: %v \n", nums)
+	fmt.Println(separator)
+
+	fmt.Printf("Array before removing duplicates: %v \n", nums_Duplicates)
+	nums_Sorted := algorithm.Remove_duplicates(nums_Duplicates)
+	fmt.Printf("Array sorted: %v \n", nums_Sorted)
+
 
 }
